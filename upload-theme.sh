0fae18eafd4b8c42a4269bb7620a45cf4dad946e
yuler <joyful4yu@gmail.com>
2020-01-10 19:48:32 +0800

    Update GitHub Action for deploy theme

---
+++ upload-theme.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# refs: https://ghost.org/docs/api/v3/admin/#token-authentication
+
+# Admin API key goes here
+KEY=$API_KEY
+
+# Split the key into ID and SECRET
+TMPIFS=$IFS
+IFS=':' read ID SECRET <<< "$KEY"
+IFS=$TMPIFS
+
+# Prepare header and payload
+NOW=$(date +'%s')
+FIVE_MINS=$(($NOW + 300))
+HEADER="{\"alg\": \"HS256\",\"typ\": \"JWT\", \"kid\": \"$ID\"}"
+PAYLOAD="{\"iat\":$NOW,\"exp\":$FIVE_MINS,\"aud\": \"/v3/admin/\"}"
+
+# Helper function for perfoming base64 URL encoding
+base64_url_encode() {
+    declare input=${1:-$(</dev/stdin)}
+    # Use `tr` to URL encode the output from base64.
+    printf '%s' "${input}" | base64 | tr -d '=' | tr '+' '-' |  tr '/' '_'
+}
+
+# Prepare the token body
+header_base64=$(base64_url_encode "$HEADER")
+payload_base64=$(base64_url_encode "$PAYLOAD")
+
+header_payload="${header_base64}.${payload_base64}"
+
+# Create the signature
+signature=$(printf '%s' "${header_payload}" | openssl dgst -binary -sha256 -mac HMAC -macopt hexkey:$SECRET | base64_url_encode)
+
+# Concat payload and signature into a valid JWT token
+TOKEN="${header_payload}.${signature}"
+
+# Make an authenticated request to create a post
+curl -H "Authorization: Ghost $TOKEN" \
+-X POST \
+-F 'file=@./dist/mymarianas-ghost-theme.zip' \
+-F 'purpose=themes' \
+"$API_URL/ghost/api/v3/admin/themes/upload"
