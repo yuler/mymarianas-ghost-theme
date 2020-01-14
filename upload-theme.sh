caed2e36bf10ba1e457c5a370f6fdb706069c7e1
yuler <joyful4yu@gmail.com>
2020-01-14 11:24:31 +0800

    Update upload-theme.sh & delete asets/built/*

--- upload-theme.sh
+++ upload-theme.sh
@@ -17,9 +17,10 @@ PAYLOAD="{\"iat\":$NOW,\"exp\":$FIVE_MINS,\"aud\": \"/v3/admin/\"}"
 
 # Helper function for perfoming base64 URL encoding
 base64_url_encode() {
-    declare input=${1:-$(</dev/stdin)}
+    # declare input=${1:-$(</dev/stdin)}
     # Use `tr` to URL encode the output from base64.
-    printf '%s' "${input}" | base64 | tr -d '=' | tr '+' '-' |  tr '/' '_'
+    # printf '%s' "${input}" | base64 | tr -d '=' | tr '+' '-' |  tr '/' '_'
+    echo -n "$1" | openssl enc -a -A | tr -d '=' | tr '/+' '_-'
 }
 
 # Prepare the token body
@@ -29,14 +30,19 @@ payload_base64=$(base64_url_encode "$PAYLOAD")
 header_payload="${header_base64}.${payload_base64}"
 
 # Create the signature
-signature=$(printf '%s' "${header_payload}" | openssl dgst -binary -sha256 -mac HMAC -macopt hexkey:$SECRET | base64_url_encode)
-
+# signature=$(printf '%s' "${header_payload}" | openssl dgst -binary -sha256 -mac HMAC -macopt hexkey:$SECRET | base64_url_encode)
+signature=$(printf '%s' "${header_payload}" | openssl dgst -binary -sha256 -mac HMAC -macopt hexkey:$SECRET)
+signature=$(base64_url_encode "$signature")
 # Concat payload and signature into a valid JWT token
 TOKEN="${header_payload}.${signature}"
 
 # Make an authenticated request to create a post
-curl -H "Authorization: Ghost $TOKEN" \
--X POST \
--F 'file=@./dist/mymarianas-ghost-theme.zip' \
--F 'purpose=themes' \
-"$API_URL/ghost/api/v3/admin/themes/upload"
+response=$(curl -H "Authorization: Ghost $TOKEN" -X POST -F 'file=@./dist/mymarianas-ghost-theme.zip' -F 'purpose=themes' "$API_URL/ghost/api/v3/admin/themes/upload")
+
+echo $response
+
+if [[ $response =~ "mymarianas-ghost-theme" ]]; then
+  exit 0
+else 
+  exit 1
+fi
